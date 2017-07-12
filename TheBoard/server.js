ce35d80297381e9bb570f166a2c58919a7082a17
almogaya1315 <liormatsliah1985@gmail.com>
2017-07-12 20:37:52 +0300

    flow

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -9,8 +9,17 @@ var http = require("http");
 var express = require("express");
 var app = express();
 
+//set-up the view engine
+app.set("view engine", "jade");
+
 app.get("/", function (req, res) {
-    res.send("<html><body><h1>Express</h1></body></html>");
+    //res.send("<html><body><h1>Express</h1></body></html>");
+    res.render("jade/index", { title: "Express + Jade" });
+});
+
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Lior", surname: "Matsliah" });
 });
 
 var server = http.createServer(app);
